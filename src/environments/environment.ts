e89ac5e553067bf6f9024366ba0c6e4efdaee62a
workhard2021 <csouleymane025@gmail.com>
2024-10-16 17:05:57 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
     APP_CLIENT:{auth: {
         clientId: 'eb695b1b-68c0-471b-a942-9c85da7bfec0',
         authority: 'https://login.microsoftonline.com/4d24a5e1-43ac-47f2-8666-7018b13dec8a',
-        redirectUri: 'http://localhost:4200/',
+        // redirectUri: 'http://localhost:4200/',
+        redirectUri:'https://powerbi-mu.vercel.app'
     },
     cache: {
         cacheLocation: "localStorage",
